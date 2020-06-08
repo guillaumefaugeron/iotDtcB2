084503591470f1a5c5631f951a3a95cea8feb153
Arnaud PIZZETTA <arnaudpizzetta@live.com>
2020-06-08 12:31:41 +0200

    fix merge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 import wiotp.sdk.application
+import requests
+
 
 def myCommandCallback(cmd):
     print("Command received: %s" % cmd.data)
@@ -8,24 +10,39 @@ def myEventCallback(event):
     str = "%s event '%s' received from device [%s]: %s"
     print(str % (event.format, event.eventId, event.device, json.dumps(event.data)))
 
-
-# Subrcribe to all DTC devices
-def AdminSubscribeToAll():
-    print("Subscribing to all DTC devices...")
-    client.subscribeToDeviceEvents(typeId="DTC",eventId="CitizenStatus")
-
-myConfig = wiotp.sdk.application.parseConfigFile("default-config.yaml")
+# init
+myConfig = wiotp.sdk.application.parseConfigFile("config.yaml")
 client = wiotp.sdk.application.ApplicationClient(config=myConfig, logHandlers=None)
 client.deviceEventCallback = myEventCallback
-
 client.commandCallback = myCommandCallback
 
 
-client.connect()
-AdminSubscribeToAll()
+create_account = input("voulez-vous creer un compte (O/n) : ")
 
-print(client.subscriptionCallback)
-while 1:
-    a=1
+if (create_account.lower() == "o") | (create_account.lower() == "oui") :
+    create = 0
+    while create != 1:
+        url_api = "https://vkrqyu.internetofthings.ibmcloud.com/api/v0002/"
+        url_device_DTC = url_api + "device/types/DTC/devices"
+        headers = {'content-type': 'application/json'}
+        device_id = input("rentrer le nom du device : ")
+
+        myobj = """{"deviceId": "%s"}""" %device_id
+        request = requests.post(url_device_DTC, data = myobj, headers=headers,  auth=('a-vkrqyu-lfcnfan1no', '7ubBV0eEX_SAa&)?rN'))
+
+        if request.status_code == 201:
+            create = 1
+            print(request.text)
+        else:
+            print(request.text)
+
+
+# Connect
+client.connect()
+
+
+
+# client.subscribeToDeviceEvents(typeId="DTC",eventId="contact")
+
 
 client.disconnect()
